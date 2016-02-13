b6f0ee70a357e8d8fe4441ab99553f571febc2b2
Romain Lenzotti <rom.lenzotti@gmail.com>
2016-02-13 12:29:48 +0100

    Remove Grunt tasks. Use npm install and/or test to build or test module

--- test/spec/httpException.spec.ts
+++ test/spec/httpException.spec.ts
@@ -1,5 +1,6 @@
 import * as Chai from 'chai';
-import HTTPException = require("../../lib/http-exception");
+import * as HTTPException from "../../index";
+
 var expect = Chai.expect;
 
 
@@ -12,162 +13,200 @@ describe('HTTPExceptions', function(){
 
     });
 
+    describe('BadGateway', function(){
+        it('should emit an exception', function(){
 
-    it('should emit a BadGateway exception', function(){
+            try{
+                throw new HTTPException.BadGateway('message');
+            }catch(err){
+                expect(err instanceof HTTPException.HTTPException).to.be.true;
+                expect(err.status).to.equal(502);
+                expect(err.toString()).to.equal('BAD_GATEWAY(502): message');
+            }
 
-        try{
-            throw new HTTPException.BadGateway('message');
-        }catch(err){
-            expect(err instanceof HTTPException.HTTPException).to.be.true;
-            expect(err.status).to.equal(502);
-            expect(err.toString()).to.equal('BAD_GATEWAY(502): message');
-        }
-
-    });
-
-    it('should emit a BadRequest exception', function(){
-
-        try{
-            throw new HTTPException.BadRequest('Parameters required [t1,t2]' );
-        }catch(err){
-            expect(err instanceof HTTPException.HTTPException).to.be.true;
-            expect(err.status).to.equal(400);
-            expect(err.toString()).to.equal('BAD_REQUEST(400): Parameters required [t1,t2]');
-        }
-
-    });
-
-    it('should emit a Forbidden', function(){
-
-        try{
-            throw new HTTPException.Forbidden('message');
-        }catch(err){
-
-            expect(err instanceof HTTPException.HTTPException).to.be.true;
-            expect(err.status).to.equal(403);
-            expect(err.toString()).to.equal('FORBIDDEN(403): message');
-        }
-
-    });
-
-    it('should emit a GatewayTimeout exception', function(){
-
-        try{
-            throw new HTTPException.GatewayTimeout('message');
-        }catch(err){
-
-            expect(err instanceof HTTPException.HTTPException).to.be.true;
-            expect(err.status).to.equal(400);
-        }
-
-    });
-
-    it('should emit a InternalServerError exception', function(){
-
-        try{
-            throw new HTTPException.InternalServerError('message');
-        }catch(err){
-
-            expect(err instanceof HTTPException.HTTPException).to.be.true;
-            expect(err.status).to.equal(500);
-        }
-
-    });
-
-    it('should emit a MethodNotAllowed exception', function(){
-
-        try{
-            throw new HTTPException.MethodNotAllowed('message');
-        }catch(err){
-
-            expect(err instanceof HTTPException.HTTPException).to.be.true;
-            expect(err.status).to.equal(405);
-        }
-
-    });
-
-    it('should emit a NotAcceptable exception', function(){
-
-        try{
-            throw new HTTPException.NotAcceptable('message');
-        }catch(err){
-
-            expect(err instanceof HTTPException.HTTPException).to.be.true;
-            expect(err.status).to.equal(406);
-        }
-
+        });
     });
 
 
-    it('should emit a NotFound exception', function(){
+    describe('BadRequest', function() {
 
-        try{
-            throw new HTTPException.NotFound('message');
-        }catch(err){
+        it('should emit an exception', function () {
 
-            expect(err instanceof HTTPException.HTTPException).to.be.true;
-            expect(err.status).to.equal(404);
-        }
+            try {
+                throw new HTTPException.BadRequest('Parameters required [t1,t2]');
+            } catch (err) {
+                expect(err instanceof HTTPException.HTTPException).to.be.true;
+                expect(err.status).to.equal(400);
+                expect(err.toString()).to.equal('BAD_REQUEST(400): Parameters required [t1,t2]');
+            }
 
+        });
     });
 
-    it('should emit a NotImplemented exception', function(){
 
-        try{
-            throw new HTTPException.NotImplemented('message');
-        }catch(err){
+    describe('Forbidden', function() {
+        it('should emit an exception', function () {
 
-            expect(err instanceof HTTPException.HTTPException).to.be.true;
-            expect(err.status).to.equal(501);
-        }
+            try {
+                throw new HTTPException.Forbidden('message');
+            } catch (err) {
 
+                expect(err instanceof HTTPException.HTTPException).to.be.true;
+                expect(err.status).to.equal(403);
+                expect(err.toString()).to.equal('FORBIDDEN(403): message');
+            }
+
+        });
     });
 
-    it('should emit a PaymentRequired exception', function(){
+    describe('GatewayTimeout', function() {
 
-        try{
-            throw new HTTPException.PaymentRequired('message');
-        }catch(err){
+        it('should emit an exception', function () {
 
-            expect(err instanceof HTTPException.HTTPException).to.be.true;
-            expect(err.status).to.equal(402);
-        }
+            try {
+                throw new HTTPException.GatewayTimeout('message');
+            } catch (err) {
 
+                expect(err instanceof HTTPException.HTTPException).to.be.true;
+                expect(err.status).to.equal(504);
+            }
+
+        });
     });
 
-    it('should emit a ProxyError exception', function(){
+    describe('InternalServerError', function() {
 
-        try{
-            throw new HTTPException.ProxyError('message');
-        }catch(err){
+        it('should emit an exception', function () {
 
-            expect(err instanceof HTTPException.HTTPException).to.be.true;
-            expect(err.status).to.equal(502);
-        }
+            try {
+                throw new HTTPException.InternalServerError('message');
+            } catch (err) {
 
+                expect(err instanceof HTTPException.HTTPException).to.be.true;
+                expect(err.status).to.equal(500);
+            }
+
+        });
     });
 
-    it('should emit a ServiceUnvailable exception', function(){
+    describe('MethodNotAllowed', function() {
 
-        try{
-            throw new HTTPException.ServiceUnvailable('message');
-        }catch(err){
+        it('should emit an exception', function () {
 
-            expect(err instanceof HTTPException.HTTPException).to.be.true;
-            expect(err.status).to.equal(503);
-        }
+            try {
+                throw new HTTPException.MethodNotAllowed('message');
+            } catch (err) {
 
+                expect(err instanceof HTTPException.HTTPException).to.be.true;
+                expect(err.status).to.equal(405);
+            }
+
+        });
     });
 
-    it('should emit a Unauthorized exception', function(){
+    describe('NotAcceptable', function() {
 
-        try{
-            throw new HTTPException.Unauthorized('message');
-        }catch(err){
+        it('should emit an exception', function () {
 
-            expect(err instanceof HTTPException.HTTPException).to.be.true;
-            expect(err.status).to.equal(401);
-        }
+            try {
+                throw new HTTPException.NotAcceptable('message');
+            } catch (err) {
 
+                expect(err instanceof HTTPException.HTTPException).to.be.true;
+                expect(err.status).to.equal(406);
+            }
+
+        });
+    });
+
+
+    describe('NotFound', function() {
+
+        it('should emit an  exception', function () {
+
+            try {
+                throw new HTTPException.NotFound('message');
+            } catch (err) {
+
+                expect(err instanceof HTTPException.HTTPException).to.be.true;
+                expect(err.status).to.equal(404);
+            }
+
+        });
+    });
+
+    describe('NotImplemented', function() {
+
+        it('should emit an exception', function () {
+
+            try {
+                throw new HTTPException.NotImplemented('message');
+            } catch (err) {
+
+                expect(err instanceof HTTPException.HTTPException).to.be.true;
+                expect(err.status).to.equal(501);
+            }
+
+        });
+    });
+
+    describe('PaymentRequired', function() {
+
+        it('should emit an exception', function () {
+
+            try {
+                throw new HTTPException.PaymentRequired('message');
+            } catch (err) {
+
+                expect(err instanceof HTTPException.HTTPException).to.be.true;
+                expect(err.status).to.equal(402);
+            }
+
+        });
+    });
+
+    describe('ProxyError', function() {
+
+        it('should emit an exception', function () {
+
+            try {
+                throw new HTTPException.ProxyError('message');
+            } catch (err) {
+
+                expect(err instanceof HTTPException.HTTPException).to.be.true;
+                expect(err.status).to.equal(502);
+            }
+
+        });
+    });
+
+    describe('ServiceUnvailable', function() {
+
+        it('should emit an exception', function () {
+
+            try {
+                throw new HTTPException.ServiceUnvailable('message');
+            } catch (err) {
+
+                expect(err instanceof HTTPException.HTTPException).to.be.true;
+                expect(err.status).to.equal(503);
+            }
+
+        });
+    });
+
+    describe('Unauthorized', function() {
+
+        it('should emit an exception', function () {
+
+            try {
+                throw new HTTPException.Unauthorized('message');
+            } catch (err) {
+
+                expect(err instanceof HTTPException.HTTPException).to.be.true;
+                expect(err.status).to.equal(401);
+            }
+
+        });
     });
 });
